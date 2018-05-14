6ce057424151df563a159993dbd8047356eb41ea
qianlongdoit <qianlong123321@126.com>
2018-05-14 09:58:06 +0800

    378 add comment

--- 378. 有序矩阵中第K小的元素.js
+++ 378. 有序矩阵中第K小的元素.js
@@ -15,30 +15,34 @@
  * 返回 13
  */
 
-/**写一个方法获取(i,j)位置小于其的数的个数
+/**用二分法找到符合条件的数。 但为什么二分就一定可以找到？
+ *
+ * 因为只有逼近矩阵中的数的时候，getCountLessThan的返回值才会有变化，
  *
  * @param {number[][]} matrix
  * @param {number} k
  * @return {number}
  */
 var kthSmallest = function (matrix, k) {
+
+  if (matrix.length === 1) return matrix[0][0];
+
   let len = matrix.length;
   let min = matrix[0][0];
   let max = matrix[len - 1][len - 1];
-  if (matrix.length === 1) return matrix[0][0];
 
   while (min <= max) {
-    let mid = (max + min) >> 1;
+    let mid = (max + min) >> 1;// 右移一位，即取平均数
     let count = getCountLessThan(matrix, mid);
 
     count < k ? min = mid + 1 : max = mid - 1;
     console.log(min, mid, max, count, k)
   }
 
-  return min
+  return min;
 };
 
-/**在题中的矩阵中找到所以比target小的数的数量
+/**在题中的矩阵中找到所有比target小的数的数量
  *
  * @param matrix
  * @param target
@@ -51,7 +55,7 @@ function getCountLessThan(matrix, target) {
   let y = 0;
 
   while (x >= 0 && y < len) {
-    //找到当前行第一个大于target的数的index
+    //找到当前列第一个大于target的数的index
     matrix[x][y] > target ? x-- : (count += x + 1, y++);
   }
 
@@ -61,10 +65,10 @@ function getCountLessThan(matrix, target) {
 
 let matrix = [
   [1, 5, 9],
-  [10, 11, 13],
-  [12, 13, 15]
+  [10, 11, 16],
+  [12, 16, 100]
 ];
 
 // matrix = [[2000000000]]
-console.log(kthSmallest(matrix, 1))
+console.log(kthSmallest(matrix, 9))
 // console.log(getCountLessThan(matrix, 14))
