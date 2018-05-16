f7b633d61aa2010f34d8878cda22e546a86b3ed1
ql <qianlong123321@126.com>
2018-05-16 21:02:20 +0800

    add 053

--- 053. 最大子序和.js
+++ 053. 最大子序和.js
@@ -12,13 +12,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-  let maxSum = 0;
+var maxSubArray = function (nums) {
+  let maxSum = nums[0];
   let current = 0;
 
-  for (let i = 0, len = nums.length;i < len; i++) {
-    if (nums[i] < 0 && current <= 0) continue;
-    maxSum += nums[i]
+  for (let i = 0, len = nums.length; i < len; i++) {
+    current < 0 ? current = nums[i] : current += nums[i];
+    maxSum = Math.max(current, maxSum);
   }
-  
+
+  return maxSum
 };
+
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// let arr = [-2, -1, -3, -4, -1, -2, -1, -5, -4];
+
+console.log(maxSubArray(arr))
