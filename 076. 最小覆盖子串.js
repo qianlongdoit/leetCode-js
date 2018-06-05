5060be8d5a2c9c264c6bc375a63707593094a56f
qianlongdoit <qianlong123321@126.com>
2018-06-05 17:45:21 +0800

    add 076

--- 076. 最小覆盖子串.js
+++ 076. 最小覆盖子串.js
@@ -12,7 +12,7 @@
  */
 
 /**此题归类到哈希表和双指针问题，
- * 拓展： 如果t中有重复字符怎么办？ => 重复几次则数量多于几
+ * 拓展： 如果t中有重复字符怎么办？ => 设定一个标准记录应该出现的次数
  * 思路：
  *    两个指针slow、fast从0开始移动；
  *    用一个map存slow-fast之间包含t中字符的次数；
@@ -21,64 +21,82 @@
  *    slow开始移动，slow每次碰到t中字符则对应数量--；
  *    异动前判断，如果对应数量大于0，则可以移动，否则fast移动；
  *
+ * 策略想出来了，但是写出来花了好长时间。。。
+ *
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
-var minWindow = function(s, t) {
+var minWindow = function (s, t) {
   let count = 0;  //用于判断slow-fast是否已经包含t了
   let slow = 0;
   let fast = 0;
   let map = {};
-  let result = '';
+  let standard = {};
+  let result = s;
 
   for (let i = 0, len = s.length; i < len; i++) {
     //  s[i]在t中
     if (t.indexOf(s[i]) !== -1) {
-      !map[s[i]] ? map[s[i]] = 0 : map[s[i]]++;
+      if (map[s[i]] === undefined) map[s[i]] = 0;
     }
   }
-  let standard = Object.assign({}, map); //复制一份作为标准
+  for (let i = 0, len = t.length; i < len; i++) {
+    standard[t[i]] === undefined ? standard[t[i]] = 1 : standard[t[i]]++;
+  }
 
-  while (slow <= fast) {
-    //  开始slow移动到包含在t中的字符串的位置
-    while (map[s[slow]] === 'undefined') {
+  //  初始化slow fast的位置
+  while (map[s[fast]] === undefined && fast < s.length) {
+    fast++;
+  }
+  if (fast < s.length) {
+    slow = fast;
+    map[s[fast]] = 1;
+    count = 1;
+  } else {
+    return '';
+  }
+
+  while (slow <= fast && fast < s.length) {
+    //  如果结果比上次长度小，记录此次的结果
+    if (count === t.length && fast - slow + 1 < result.length ) {
+      result = s.slice(slow, fast + 1);
+    }
+
+    //  slow需要移动的情况
+    if (map[s[slow]] === undefined){
       slow++;
-    }
-    while (map[s[fast]] === 'undefined') {
-      fast++;
-    }
+    } else if (count === t.length && map[s[slow]] > standard[s[slow]]) {
+      map[s[slow++]]--;
+    } else {
+      //  此时才是fast需要移动的时候
+      // if (s[fast] === undefined) {
+        fast++;
+      // }
 
-    //  fast需要移动的时候
-    if (count < t.length || map[s[slow]] + 1 < standard[s[slow]]) {
-
-      fast++;
-    }
-
-    //  每次碰到t中字符时判断
-    if (map[s[fast]] !== 'undefined') {
-      //  记录slow-fast中有效t中字符的数量
-      if (map[s[fast]] <= standard[s[fast]]) {
-        count++;
-      } else if (count === t.length) {  //  如果slow-fast包含t中全部在字符
-        //  如果slow指针移动到包含t中字符处，而且该字符数量大于1，就开始移动slow指针
-        if (map[s[slow]] !== 'undefined' && map[s[slow]] > 1) {
-          slow++;
-          map[s[slow]]--;
+      if (map[s[fast]] !== undefined) {
+        if (map[s[fast]] < standard[s[fast]]) {
+          count++
         }
-
+        map[s[fast]]++;
       }
+    }
 
-      //  如果当前slow-fast是否包含t
-      if (count === t.length) {
-        let temp =  s.slice(slow, fast + 1);
-        result = result.length <  temp.length ? temp : result;
-      }
-      map[s[fast]]++;
+    console.log(count, '---------', map, slow, fast, '----------', map[s[slow]], standard[s[slow]], s.slice(slow, fast + 1))
 
+
+    //  终止条件: fast无法移动 且 slow无法移动
+    if (fast >= s.length && (slow >= s.length || map[s[slow]] <= standard[s[slow]])) {
+      if (count < t.length) return '';
+      break;
     }
   }
 
+  return result;
 };
 
-
+// console.log(minWindow("ADOBECODEBANC", "ABC"))
+// console.log(minWindow("abcccc", "bc"))
+// console.log(minWindow("bba", "ab"))
+console.log(minWindow("bba", "f"))
+// console.log(minWindow("dcabefgecdaecf", "cae"))
