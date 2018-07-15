6fca34f2a3a848d87be0bc5ec817341051ef0621
ql <qianlong123321@126.com>
2018-07-16 00:06:51 +0800

    finished 039

--- 039. 组合总和.js
+++ 039. 组合总和.js
@@ -5,47 +5,76 @@
  */
 var combinationSum = function (candidates, target) {
     let result = [];
-    let start = 0;
     let arr= [];
-    let n = candidates.length;
-    let addLen = true;
-    let sum = 0;
 
     candidates.sort( (a, b) => a - b);
-    while (start < n) {
+    arr[0] = candidates[0];
+
+    let sum = candidates[0];
+    while (arr.length) {
         if (sum < target) {
-            sum += candidates[0];
-            arr.push(candidates[0]);
+            sum += arr[arr.length - 1];
+            arr.push(arr[arr.length - 1]);
         }
 
         if (sum === target) {
-            result.push(arr);
+            // console.log(arr)
+            result.push(arr.concat([]));
             arr.pop();
             arr = nextNum(arr, candidates);
+            if (arr.length === 0) break;
             sum = arr.reduce((accu, cur) => accu + cur);
         }
 
         if (sum > target) {
             arr.pop();
             arr = nextNum(arr, candidates);
+            if (arr.length === 0) break;
             sum = arr.reduce((accu, cur) => accu + cur);
         }
+        // console.log(arr)
 
     }
 
-
+    // console.log(result)
     return result;
+
+
+    function nextNum(arr, base) {
+        for (let i = 0, n = arr.length; i < n; i++ ) {
+            let index = base.indexOf(arr[n - 1 - i]);
+            arr.length = n - i;
+            if (index + 1 < base.length) {
+                arr[n - 1 - i] = base[index + 1];
+                return arr;
+            }
+        }
+        if (arr.length === 1 && arr[0] === base[base.length - 1]) arr = [];
+        return arr;
+    }
 };
 
-var candidates = [2, 3, 5], target = 8;
-// combinationSum(candidates, target)
+// var candidates = [2, 3, 5, 6], target = 8;
+var candidates = [2, 3, 6, 7], target = 7;
+combinationSum(candidates, target)
 
 function nextNum(arr, base) {
-
+    for (let i = 0, n = arr.length; i < n; i++ ) {
+        let index = base.indexOf(arr[n - 1 - i]);
+        arr.length = n - i;
+        if (index + 1 < base.length) {
+            arr[n - 1 - i] = base[index + 1];
+            return arr;
+        }
+    }
+    if (arr.length === 1 && arr[0] === base[base.length - 1]) arr = [];
     return arr;
 }
 
 
-var arr = [1,2,3,4,5];
-var a = arr.reduce( (accumulator, currentValue) => accumulator + currentValue )
-console.log(a)
+// var arr = [1,2,3,4,5];
+// var a = arr.reduce( (accumulator, currentValue) => accumulator + currentValue )
+// console.log(a)
+
+// nextNum([6,6], [2,3,5,6])
+// console.log(nextNum([6], [2,3,6,7]))
