6b9f7185337f25defb1a4e50cc2bfa3f143929cd
ql <qianlong123321@126.com>
2018-12-01 20:33:15 +0800

    add 111

--- 111. 二叉树的最小深度.js
+++ 111. 二叉树的最小深度.js
@@ -1,4 +1,79 @@
 /**
  * Created by admin on 2018/12/1.
  */
- 
+
+/**给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例:
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *      3
+ *     / \
+ *    9  20
+ *      /  \
+ *    15   7
+ * 返回它的最小深度  2.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**BFS广度优先搜索，遍历每一层，如果这一层有一个节点，它的左右节点都是null，那么
+ * 这个节点就是最小深度
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (root === null) return 0;
+    let depth = 1;
+    let current = [root];
+    let next = [];
+
+    while (current.length) {
+        for (let i = 0, len = current.length; i < len; i++) {
+            if (current[i].left) {
+                next.push(current[i].left)
+            }
+            if (current[i].right) {
+                next.push(current[i].right)
+            }
+
+            if (current[i].left === null && current[i].right === null) {
+                return depth;
+            }
+
+        }
+
+        depth++;
+        current = next;
+        next = [];
+    }
+};
+
+let tree4 = {
+    val: 1,
+    left: {
+        val: 2,
+        left:  {
+            val: 4,
+            left:null,
+            right: null
+        },
+        right:  {
+            val: 5,
+            left:null,
+            right: null
+        }
+    },
+    right: {
+        val: 3,
+        left:null,
+        right: null
+    }
+};
+
+console.log(minDepth(tree4));
--- 886. 可能的二分法.js
+++ 886. 可能的二分法.js
@@ -1,4 +1,42 @@
 /**
  * Created by admin on 2018/12/1.
  */
- 
+
+/**给定一组 N 人（编号为 1, 2, ..., N）， 我们想把每个人分进任意大小的两组。
+ * 每个人都可能不喜欢其他人，那么他们不应该属于同一组。
+ * 形式上，如果 dislikes[i] = [a, b]，表示不允许将编号为 a 和 b 的人归入同一组。
+ * 当可以用这种方法将每个人分进两组时，返回 true；否则返回 false。
+ *
+ * 示例 1：
+ * 输入：N = 4, dislikes = [[1,2],[1,3],[2,4]]
+ * 输出：true
+ * 解释：group1 [1,4], group2 [2,3]
+ * 示例 2：
+ * 输入：N = 3, dislikes = [[1,2],[1,3],[2,3]]
+ * 输出：false
+ * 示例 3：
+ * 输入：N = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+ * 输出：false
+ *
+ * 提示：
+ * 1 <= N <= 2000
+ * 0 <= dislikes.length <= 10000
+ * 1 <= dislikes[i][j] <= N
+ * dislikes[i][0] < dislikes[i][1]
+ * 对于 dislikes[i] == dislikes[j] 不存在 i != j
+ */
+
+/**无向图问题，待学习后解答
+ * @param {number} N
+ * @param {number[][]} dislikes
+ * @return {boolean}
+ */
+var possibleBipartition = function(N, dislikes) {
+    let remain = Array.from(new Array(N),(val,index)=>index+1);
+    let group1 = [];
+    let group2 = [];
+
+    for (let i = 0, len = dislikes.length; i < len; i++) {
+
+    }
+};
