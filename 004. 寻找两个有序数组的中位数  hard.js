91c045a04060531795dfe414bafb63d803514c8a
ql <qianlong123321@126.com>
2019-03-17 16:35:19 +0800

    add 004

--- 004. 寻找两个有序数组的中位数  hard.js
+++ 004. 寻找两个有序数组的中位数  hard.js
@@ -43,43 +43,74 @@
  * 在num1 和 num2数组中一定不存一个数在 num1[c1] 和 num1[c2]之间
  * 且c1 + c2就是中位数的index，即长度的一半
  *
+ * 上面的思路有个很大的漏洞。。。
+ * 那就是没考虑到奇数的情况，也就是这个中位数可能只在num1或者num2中的一个数
+ *
+ * 换个思路
+ * 我们现在已知在数据流中取中位数的解法
+ * 维持2个堆，一个大根堆一个小根堆，2个堆的大小差不超过1，
+ * 最大堆的堆首一定小于最小堆的堆首
+ * 这样每次的中位数不是大根堆的堆首就是两个堆首的平均数
+ *
+ * 回到本题，2个堆已经建立了，那么剩下的就是调节2个堆的大小以及调整堆首的元素了
+ * 但是这个算法的时间复杂度是多少，已经计算不出来了。。。
+ *
  *
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    let len1 = nums1.length;
-    let len2 = nums2.length;
-    let c1 = 0;
-    let c2 = 0;
+    //保持nums1长度比nums2的长度长
+    if (nums1.length < nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
 
-    let medianIndex = Math.ceil(len1 + len2);
-
-    while (c1 + c2 !== medianIndex) {
-        console.log(c1, c2);
-        //  需要向尾部移动
-        if (c1 + c1 < medianIndex) {
-            //  谁小谁移动
-            if (nums1[c1] <= nums2[c2]) {
-                c1 = ~~((c1 + len1) / 2);
+    const justifyHeap = (arr, flag) => {
+        if (!arr.length) return;
+        let i = 0;
+        let left = 2 * i + 1;
+        while (left < arr.length) {
+            let max;
+            if (!flag) {
+                max = arr[left + 1] > arr[left] && left + 1 < arr.length ? left + 1 : left;
+                max = arr[i] < arr[max] ? max : i;
             } else {
-                c2 = ~~((c2 + len2) / 2);
+                max = arr[left + 1] < arr[left] && left + 1 < arr.length ? left + 1 : left;
+                max = arr[i] < arr[max] ? i : max;
             }
-        } else {
-            //需要向头部移动
-            if (nums1[c1] <= nums2[c2]) {
-                c1 = ~~(c1 / 2);
-            } else {
-                c2 = ~~(c2 / 2);
-            }
+            if (i === max) break;
+            [arr[i], arr[max]] = [arr[max], arr[i]];
+            i = max;
+            left = 2 * i + 1;
         }
     }
 
-    console.log(c1, c2);
+    nums1 = nums1.reverse();
+    //调整堆的size
+    while (nums1.length - nums2.length > 1) {
+        let current = nums1.shift();
+        nums2.unshift(current);
+        // justifyHeap(nums1);
+        justifyHeap(nums2, true);
+    }
+
+    //调整2个堆的首元素
+    while (nums1[0] > nums2[0]) {
+        [nums1[0], nums2[0]] = [nums2[0], nums1[0]];
+        justifyHeap(nums1);
+        justifyHeap(nums2, true);
+    }
+
+    return !((nums1.length + nums2.length) % 2 ) ? (nums1[0] + nums2[0]) / 2 : nums1[0];
 };
 
-let num1 = [1, 3];
-let num2 = [2];
-findMedianSortedArrays(num1, num2)
+let num1 = [1, 2, 4, 7, 9, 10];
+let num2 = [2, 2, 3, 4, 7, 8];
+
+// num1 = [1, 3]
+// num2 = [2]
+num1 = [6]
+num2 = [1,2,3,4,5,7,8]
+console.log(findMedianSortedArrays(num1, num2));
 
