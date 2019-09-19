bc92ab54693caaadc9e1be4808fb6a1c73fcb39b
ql <qianlong123321@126.com>
2019-09-20 00:10:37 +0800

    add: 278

---
+++ 278. 第一个错误的版本 easy.js
@@ -0,0 +1,64 @@
+/**
+ * Created by doit on 2019/9/19.
+ */
+
+
+/**你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。
+ * 由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。
+ * 实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ *
+ *  给定 n = 5，并且 version = 4 是第一个错误的版本。
+ *
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ * 所以，4 是第一个错误的版本。
+ */
+
+
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 1;
+        let end = n;
+        let mid = ~~((start + end) / 2);
+
+        while (start + 1 < end) {
+            if (isBadVersion(mid)) {
+                end = mid;
+                mid = ~~((start + mid) / 2);
+            } else {
+                start = mid;
+                mid = ~~((end + mid) / 2);
+            }
+        }
+        return isBadVersion(mid) ? mid : end;
+    };
+};
+
+function isBadVersion(n) {
+    return n >= 1;
+}
+
+console.log(solution(isBadVersion)(2));
+
+//13477102596
